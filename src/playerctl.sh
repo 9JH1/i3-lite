a71aa39007c4e271c76804fdc2cfec7da9b87901
= <tkf.x1os@gmail.com>
2025-05-27 12:51:40 +1200

    fixed playerctl global control binds

--- src/playerctl.sh
+++ src/playerctl.sh
@@ -7,7 +7,12 @@ if [[ "$(playerctl metadata --format '-' 2>/dev/null)" == *-* ]]; then
 	artist=$(echo "$raw_artist" | awk '{if(length > 20) printf "%.20s...\n", $0; else print}')
 	title=$(echo "$raw_title" | awk '{if(length > 20) printf "%.20s...\n", $0; else print}')
 
-	echo "  $artist - $title"
+	echo -e "  $artist - $title "
+	if [ $(playerctl status) == "Playing" ]; then
+		echo ""
+	else 
+		echo ""
+	fi
 
 else 
 	echo "󰝛 "
